c71ca634f3bec32b54e7e5514373d284abefbcaa
Anil Nautiyal <nautiyal.anil@gmail.com>
2023-03-06 09:58:26 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.sap.ui.projfrmtemp.projfromtemplate.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
